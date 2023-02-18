627c96a9d21b67593b13664fd619ee41cd986f0a
Ryan Flynn <ryanflynn44@gmail.com>
2023-02-18 16:09:08 -0500

    Added menu bars for mobile

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -62,6 +62,44 @@ const Navbar = ({ selectedPage, setSelectedPage }: any) => {
           <img alt='menu-icon' src='../assets/menu-icon.svg' />
         </button>
         )}
+
+      { !isAboveSmallScreens && isMenuToggled && (
+        <div className='fixed right-0 bottom-0 h-full bg-blue w-[300px]'>
+          <div className='flex justify-end p-12'>
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <img alt='close-icon' src='../assets/close-icon.svg' />
+            </button>
+          </div>
+
+        <div className='flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue'>
+        <Link
+              page='Home'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Skills'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Projects'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Testimonials'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Contact'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+        </div>
+        </div>
+      )}
       </div>
     </nav>
   );
