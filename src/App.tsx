017baf93513614b6bc0d259a25f7c40033cae9ec
Ryan Flynn <ryanflynn44@gmail.com>
2023-02-18 15:17:27 -0500

    Basic nav done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
+  return (
+    <div className="app bg-deep-blue">
+      <header className="App-header">
+        <Navbar selectedPage={selectedPage} setSelectedPage{...setSelectedPage} />
       </header>
     </div>
   );
---
+++ src/scenes/Navbar.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import useMediaQuery from '../hooks/useMediaQuery';
+
+const Link = ({ page, selectedPage, setSelectedPage }: any) => {
+  const lowerCasePage = page.toLowerCase();
+  const yellowOrWhite = selectedPage === lowerCasePage ? 'text-yellow' : 'text-white';
+  return (
+    <AnchorLink 
+      className={`${yellowOrWhite}
+        hover:text-yellow`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
+const Navbar = ({ selectedPage, setSelectedPage }: any) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
+  console.log(isAboveSmallScreens);
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className='flex items-center justify-between mx-auto w-5/6'>
+        <h4 className="font-inter text-3xl font-bold">RF</h4>
+
+        {isAboveSmallScreens ? (
+          <div className='flex justify-between gap-16 font-inter text-sm font-semibold'>
+            <Link
+              page='Home'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Skills'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Projects'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Testimonials'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Contact'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+        <button
+          className='rounded-full bg-red p-2'
+          onClick={() => setIsMenuToggled(!isMenuToggled)}
+        >
+          <img alt='menu-icon' src='../assets/menu-icon.svg' />
+        </button>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
